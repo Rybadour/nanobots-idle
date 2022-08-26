b8863aa7bb0b16f452195bfe93f7b10cd8104189
rybadour <rybadour@gmail.com>
2022-08-26 15:15:46 -0400

    Nanobots spawn at the center of the factory

--- src/field/factory.ts
+++ src/field/factory.ts
@@ -1,5 +1,5 @@
 import Assets from "assets";
-import createjs from "createjs-module";
+import createjs, { Point } from "createjs-module";
 
 import { Bonuses } from "../shared/types";
 import { centerDisplayOn } from "../shared/utils";
@@ -7,12 +7,14 @@ import { centerDisplayOn } from "../shared/utils";
 const SPAWN_RATE = 4;
 
 class Factory {
+  pos: Point;
   display: createjs.DisplayObject;
 
   nextSpawn: number = 0;
   spawnBot: (x: number, y: number) => void;
 
   constructor(x: number, y: number, assets: Assets, spawnBot: (x: number, y: number) => void) {
+    this.pos = new Point(x, y);
     this.display = assets.getSprite('nano-core');
     centerDisplayOn(x, y, this.display);
 
@@ -25,7 +27,7 @@ class Factory {
       return;
     }
 
-    this.spawnBot(this.display.x, this.display.y);
+    this.spawnBot(this.pos.x, this.pos.y);
     this.nextSpawn = SPAWN_RATE;
   }
 
